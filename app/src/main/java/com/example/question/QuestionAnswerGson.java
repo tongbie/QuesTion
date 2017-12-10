7f691c0b59fff00fa14a06da5229ab05f759e710
tongbie <2489550615@qq.com>
2017-12-10 16:57:43 +0800

    提交答案

---
+++ app/src/main/java/com/example/question/QuestionAnswerGson.java
@@ -0,0 +1,26 @@
+package com.example.question;
+
+/**
+ * Created by aaa on 2017/12/10.
+ */
+
+public class QuestionAnswerGson {
+    private String QuestionId;
+    private String Answer;
+
+    public String getQuestionId() {
+        return QuestionId;
+    }
+
+    public void setQuestionId(String QuestionId) {
+        this.QuestionId = QuestionId;
+    }
+
+    public String getAnswer() {
+        return Answer;
+    }
+
+    public void setAnswer(String Answer) {
+        this.Answer = Answer;
+    }
+}
